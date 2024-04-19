eb107690aaae5ffbfbe391ae45c7866bdf20dd65
AlessioGallozzi <alessio.gallozzi1412@gmail.com>
2024-04-19 11:55:06 +0200

    modifiche

---
+++ ex1.py
@@ -0,0 +1,5 @@
+name = "Alessio"
+Age=20
+print (f"my name is {name} and I am {age} years old.")
+
+
---
+++ ex2.py
@@ -0,0 +1,5 @@
+altezza=3
+lunghezza=6
+area=altezza*lunghezza
+perimetro=(altezza*2)+(lunghezza*2)
+print(f"l'area è {area} il perimetro è {perimetro}")
